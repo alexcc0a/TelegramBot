86d9afd351f992fe7d294fddc8e7d56c2b26b9ef
Alexander Nesterov <alex18nest@icloud.com>
2024-08-11 13:23:33 -0300

    [+] TelegramBot

---
+++ README.md
@@ -0,0 +1,7 @@
+To send a request to NASA and receive a picture of space.
+
+1. We get the key for the NASA API at: https://api.nasa.gov/
+2. We make a request from the code: https://api.nasa.gov/planetary/apod?api_key=ВАШ_КЛЮЧ
+3. Connecting the Telegram API dependency
+
+<img width="641" alt="Снимок экрана 2024-01-04 в 16 30 07" src="https://github.com/alexcc0a/TelegramBot/assets/94784081/b5d71a91-f0e4-45c4-adc7-8846a03fd6ef">
---
+++ src/main/java/MyTelegramBot.java
@@ -0,0 +1,69 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.IOException;
+
+public class MyTelegramBot extends TelegramLongPollingBot {
+    public static final String BOT_TOKEN = "";
+
+    public static final String BOT_USERNAME = "JARVIS_Technologies_bot";
+
+    public static final String URI = "https://api.nasa.gov/planetary/apod?api_key=";
+
+    public static long chatId;
+
+    public MyTelegramBot() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            botsApi.registerBot(this);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_USERNAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            chatId = update.getMessage().getChatId();
+            switch (update.getMessage().getText()) {
+                case "/start":
+                    sendMessage("Hello, I'm a J.A.R.V.I.S. bot! I send links to pictures upon request. " +
+                            "Let me remind you that the pictures on the NASA website are updated once a day. +" +
+                            "\n" + "Enter the command /give and I will send you a picture of space");
+                    break;
+                case "/give":
+                    try {
+                        sendMessage(Utils.getUrl(URI));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void sendMessage(String messageText) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(messageText);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
